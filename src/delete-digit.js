9093e078cfe48169d6b4446d067885e8ae4a8f18
Alina M <crlin.m@gmail.com>
2025-01-02 15:26:18 +0500

    feat: implement countCats, deleteDigit functions

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const nStr = n.toString();
+  const nLength = nStr.length;
+  let res = 0;
+  for (let i = 0; i < nLength; i++) {
+    const currentStr = nStr.slice(0, i) + nStr.slice(i + 1, nLength);
+    const currentNumber = Number.parseInt(currentStr);
+    if (currentNumber > res) {
+      res = currentNumber;
+    }
+  }
+  return res;
 }
 
 module.exports = {
